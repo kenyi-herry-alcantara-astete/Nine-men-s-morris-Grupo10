eeb5514a613b27e5b531057be06802f8a237ddc3
Miller-RS <alexander.rojas.s@uni.pe>
2022-05-29 21:34:13 -0500

    agregando test moverPieza , volarPieza , validateMove

--- src/test/java/sprint2/MainFrameTest.java
+++ src/test/java/sprint2/MainFrameTest.java
@@ -58,4 +58,62 @@ class MainFrameTest {
         Assertions.assertEquals("2",myFrame.currentLogicGame.myTable[1][3]);
 
     }
+    @Test
+    void movePieceToUITest(){
+
+        MainFrame myFrame = new MainFrame();
+
+        //Condiciones Previas
+        myFrame.numberPiecesLeft = 0;
+        myFrame.numberPiecesRight = 0;
+        myFrame.player2.turn = "dos";
+        myFrame.player1.turn = "uno";
+        myFrame.numberMove = 0;
+        myFrame.player1.numberPieces=9;
+        myFrame.a1.setIcon(myFrame.IconWithPiece1) ;
+
+        //Verifiquemos cuando se llama al metodo movePieceToUI este te debe cambiar
+        // el numberMove de 0 a 1 para que se pueda efectuar el movimiento y cambiar
+        // el color de la ficha seleccionada a blanco
+        myFrame.movePieceToUI(myFrame.a1);
+        Assertions.assertEquals(1,myFrame.numberMove);
+        Assertions.assertEquals(myFrame.IconMove,myFrame.a1.getIcon());
+        //Ahora corroboramos con el mismo metodo movePiece que la ficha se pueda mover
+        //a un lugar adyasente
+        myFrame.movePieceToUI(myFrame.a4);
+        Assertions.assertEquals(1,myFrame.numberPiecesLeft);
+        Assertions.assertEquals(0,myFrame.numberMove);
+
+
+    }
+    @Test
+    void volarPieza(){
+
+        MainFrame myFrame = new MainFrame();
+
+        //Condiciones Previas
+        myFrame.numberPiecesLeft = 1;
+        myFrame.numberPiecesRight = 0;
+        myFrame.player2.turn = "dos";
+        myFrame.player1.turn = "uno";
+        myFrame.numberMove = 0;
+        myFrame.player1.numberPieces=2; // puede ser un número menor a 3 tambien
+        myFrame.insertPieceToUI(myFrame.a1); ;
+        myFrame.changeTurn();
+
+
+        //Verifiquemos cuando se llama al metodo volarPieza este te debe cambiar
+        // el numberMove de 0 a 1 para que se pueda efectuar el volarPieza y cambiar
+        // el color de la ficha seleccionada a blanco
+        myFrame.volarPieza(myFrame.a1);
+        Assertions.assertEquals(1,myFrame.numberMove);
+        Assertions.assertEquals(myFrame.IconMove,myFrame.a1.getIcon());
+        //Ahora corroboramos con el mismo metodo volarPieza que la ficha se pueda mover
+        //a cualquier parte disponible del tablero
+        myFrame.volarPieza(myFrame.g7);
+        Assertions.assertEquals(1,myFrame.numberPiecesLeft);
+        Assertions.assertEquals(0,myFrame.numberMove);
+
+
+    }
 }
